8b57b3a182e4eca9e90cbd6aa79469b0c7d4c564
Maria Paula Herrero <mpaulaherrero@gmail.com>
2023-01-13 17:47:44 -0400

    best score

--- game-connect4/src/objectOriented/classNotation/models/MinimaxMachinePlayer.mjs
+++ game-connect4/src/objectOriented/classNotation/models/MinimaxMachinePlayer.mjs
@@ -1,15 +1,15 @@
 import { MachinePlayer } from "./MachinePlayer.mjs";
-import { Console } from 'console-mpds';
+import { Line } from './Line.mjs'
+//import { logger } from "../utils/logger.mjs";
 
 export class MinimaxMachinePlayer extends MachinePlayer {
 
     static #MAX_STEPS = 4; 
     static #MAX_COST = 1;
-    static #OTHER_COST = 0;
     static #MIN_COST = -1;
+    static #MAX_LIMIT = 1000;
 
     #opositeColor
-    #console = new Console();
 
     constructor(color, board) {
         super(color, board);
@@ -22,31 +22,25 @@ export class MinimaxMachinePlayer extends MachinePlayer {
 
     setColumn(){
         let cost = this.#maximizePlay(0,this.getBoard());
-        this.#console.writeln("bestColumn: " + cost[0]);
+        //logger.info("bestColumn: " + cost[0]);
         this.getCoordinate().setColumn(cost[0]);
     }
     
     #maximizePlay(steps, board) {
-        //break
         if (this.#isEnd(steps, board)) return this.#getEndCost(board, MinimaxMachinePlayer.#MIN_COST);
-         
-        // Column, Score
-        let max = [null, MinimaxMachinePlayer.#MIN_COST];
-
-        // For all possible moves
+        let max = [undefined, MinimaxMachinePlayer.#MIN_COST];
         let emptyColumns = board.getEmptyColumns();
         for (let column of emptyColumns) {
-            let newBoard = board.clone(); // Create new board
+            let newBoard = board.clone();
             newBoard.putCoordinate(column, this.getColor());
-            //this.#console.writeln("maximizePlay steps: " + steps + ", maxCost: " + max[1] + ", column: " + column);
-            //this.#console.writeln(newBoard.toString());
-            let next_move = this.#minimizePlay(steps + 1, newBoard); // Recursive calling
-            //this.#console.writeln("maximizePlay steps: " + steps + ", maxCost: " + max[1] + ", nextMoveCost: " + next_move[1]+ ", column: " + column);
-            // Evaluate new move
-            if (max[0] == null || next_move[1] > max[1]) {
+            //logger.info("maximizePlay steps: " + steps + ", maxCost: " + max[1] + ", column: " + column);
+            //logger.info(newBoard.toString());
+            let next_move = this.#minimizePlay(steps + 1, newBoard);
+            //logger.info("maximizePlay steps: " + steps + ", maxCost: " + max[1] + ", nextMoveCost: " + next_move[1]+ ", column: " + column);
+            if (max[0] == undefined || next_move[1] > max[1]) {
                 max[0] = column;
                 max[1] = next_move[1];
-                //this.#console.writeln("maximizePlay steps: " + steps + ", newCost: " + max[1] + ", newColumn: " + max[0]);
+                //logger.info("maximizePlay steps: " + steps + ", newCost: " + max[1] + ", newColumn: " + max[0]);
             }
         }
         return max;
@@ -57,22 +51,22 @@ export class MinimaxMachinePlayer extends MachinePlayer {
         if (this.#isEnd(steps, board)) return this.#getEndCost(board, MinimaxMachinePlayer.#MAX_COST);
 
         // Column, Score
-        let min = [null, MinimaxMachinePlayer.#MAX_COST];
+        let min = [undefined, MinimaxMachinePlayer.#MAX_COST];
 
         // For all possible moves
         let emptyColumns = board.getEmptyColumns();
         for (let column of emptyColumns) {
             let newBoard = board.clone();
             newBoard.putCoordinate(column, this.#opositeColor);
-            //this.#console.writeln("minimizePlay steps: " + steps + ", minCost: " + min[1] + ", column: " + column);
-            //this.#console.writeln(newBoard.toString());
+            //logger.info("minimizePlay steps: " + steps + ", minCost: " + min[1] + ", columnCost: " + min[0] + ", column: " + column);
+            //logger.info(newBoard.toString());
             let next_move = this.#maximizePlay(steps + 1, newBoard); // Recursive calling
-            //this.#console.writeln("minimizePlay steps: " + steps + ", minCost: " + min[1] + ", nextMoveCost: " + next_move[1] + ", column: " + column);
+            //logger.info("minimizePlay steps: " + steps + ", minCost: " + min[1] + ", nextMoveCost: " + next_move[1] + ", column: " + column);
             // Evaluate new move
-            if (min[0] == null || next_move[1] < min[1]) {
+            if (min[0] == undefined || next_move[1] < min[1]) {
                 min[0] = column;
                 min[1] = next_move[1];
-                //this.#console.writeln("minimizePlay steps: " + steps + ", newCost: " + min[1] + ", newColumn: " + min[0]);
+                //logger.info("minimizePlay steps: " + steps + ", newCost: " + min[1] + ", newColumn: " + min[0]);
             }
         }
         return min;
@@ -83,11 +77,52 @@ export class MinimaxMachinePlayer extends MachinePlayer {
     }
 
     #getEndCost(board, cost){
-        let result = [null, cost];
+        let result = [undefined, cost];
         if(!board.isLastTokenInLine()){
-            result[1] = MinimaxMachinePlayer.#OTHER_COST;
+            result[1] = (this.#getBestScore(board) / MinimaxMachinePlayer.#MAX_LIMIT);
         }
-        //this.#console.writeln("end cost: " + result[1]);
+        //logger.info(`${cost ===MinimaxMachinePlayer.#MAX_COST ? 'minimizePlay':'maximizePlay'} END cost: ${result[1]}`);
         return result; 
     }
+
+    #getBestScore(board){
+        let point = 0;
+        for (let row = 0; row < board.getMaxRows() - 3; row++) {
+            for (let column = 0; column < board.getMaxColumns(); column++) {
+                point += this.#getInLinePoint(board, row, column, 1, 0);
+            }    
+        }
+        for (let row = 0; row < board.getMaxRows(); row++) {
+            for (let column = 0; column < board.getMaxColumns() - 3; column++) { 
+                point += this.#getInLinePoint(board, row, column, 0, 1); 
+            } 
+        }
+        for (let row = 0; row < board.getMaxRows() - 3; row++) {
+            for (let column = 0; column < board.getMaxColumns() - 3; column++) {
+                point += this.#getInLinePoint(board, row, column, 1, 1);
+            }            
+        }
+        for (let row = 3; row < board.getMaxRows(); row++) {
+            for (let column = 0; column <= board.getMaxColumns() - 4; column++) {
+                point += this.#getInLinePoint(board, row, column, -1, +1);
+            }
+        }
+        //logger.info(`Board.getLastTokenPoints-> Total points: ${point}`);
+        return point;
+    }
+    
+    #getInLinePoint(board, row, column, delta_y, delta_x) {
+        let point = 0;
+        //let stringLogger = `Board.isInLinePoint-> Coordination (${row},${column}) Direction (${delta_y},${delta_x})`
+        for (let i = 0; i < Line.LENGTH; i++) {
+            if (board.getToken(row, column) === this.getColor()) {
+                point += 1;
+            }
+            row += delta_y;
+            column += delta_x;
+        }
+        //logger.info(`${stringLogger} point: ${point}`);
+        return point;
+    }
+
 }
