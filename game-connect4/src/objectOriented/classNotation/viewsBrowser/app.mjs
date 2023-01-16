b8a770ddcc337d74f051e819c6f16a96afd054bc
Maria Paula Herrero <mpaulaherrero@gmail.com>
2023-01-16 16:36:05 -0400

    boton para comenzar

--- game-connect4/src/objectOriented/classNotation/viewsBrowser/app.mjs
+++ game-connect4/src/objectOriented/classNotation/viewsBrowser/app.mjs
@@ -1,5 +1,8 @@
 import { Connect4View } from '../views/Connect4View.mjs'
 import { ConsoleBrowser } from '../views/ConsoleBrowser.mjs'
 
-new Connect4View(new ConsoleBrowser()).play();
+const connect4 = new Connect4View(new ConsoleBrowser());
 
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    connect4.play();
+})
