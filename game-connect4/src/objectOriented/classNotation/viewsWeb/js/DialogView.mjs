6a3fe9f93b2aaaa4d61aeb101af6c2ee6cd28c6e
Maria Paula Herrero <mpaulaherrero@gmail.com>
2023-07-13 12:16:41 -0400

    Constant name

--- game-connect4/src/objectOriented/classNotation/viewsWeb/js/DialogView.mjs
+++ game-connect4/src/objectOriented/classNotation/viewsWeb/js/DialogView.mjs
@@ -1,20 +1,20 @@
 export class DialogView {
-    static welcomeText = 'Si quieres jugar contra la IA seleciona una columna, si no, cambia la opción y empieza otro juego';
-    static winnerText = `Victoria para`;
-    static tieText = `¡Empate!`;
-    static fullColumn = `La columna esta llena, intente con otra`;
-    static selectColumn = `Selecciona una columna`;
+    static WELCOME_TEXT = 'Si quieres jugar contra la IA seleciona una columna, si no, cambia la opción y empieza otro juego';
+    static WINNER_TEXT = `Victoria para`;
+    static TIE_TEXT = `¡Empate!`;
+    static FULL_COLUMN_TEXT = `La columna esta llena, intente con otra`;
+    static SELECT_COLUMN_TEXT = `Selecciona una columna`;
 
     static writeWelcome(){
-        this.write(DialogView.welcomeText);
+        this.write(DialogView.WELCOME_TEXT);
     }
 
     static writeTie(){
-        this.write(DialogView.tieText);
+        this.write(DialogView.TIE_TEXT);
     }
 
     static writeFullColumn(){
-        this.write(DialogView.fullColumn);
+        this.write(DialogView.FULL_COLUMN_TEXT);
     }
 
     static write(text){
@@ -22,18 +22,18 @@ export class DialogView {
     }
 
     static writeSelectColumnIfNotWelcome(){
-       this.writeIfNotWelcome(DialogView.selectColumn)
+       this.writeIfNotWelcome(DialogView.SELECT_COLUMN_TEXT)
     }
 
     static writeIfNotWelcome(text){
-        if(document.getElementById('dialog').innerHTML !== DialogView.welcomeText){
+        if(document.getElementById('dialog').innerHTML !== DialogView.WELCOME_TEXT){
             this.write(text);
         }
     }
 
     static writeWinner(element){
         const dialogElement = document.getElementById('dialog');
-        dialogElement.innerHTML=DialogView.winnerText;
+        dialogElement.innerHTML=DialogView.WINNER_TEXT;
         dialogElement.append(element);
         //dialogElement.parentNode.insertBefore(element,dialogElement.nextSibling);
     }
